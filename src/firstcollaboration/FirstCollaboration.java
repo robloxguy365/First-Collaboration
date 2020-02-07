7d4120c873190b6c9d1ff8106c288b12725bba8b
robloxguy365 <56357107+robloxguy365@users.noreply.github.com>
2020-02-07 15:43:41 -0500

    Added Hello World Method
    Added a personalized hello world method

--- src/firstcollaboration/FirstCollaboration.java
+++ src/firstcollaboration/FirstCollaboration.java
@@ -14,8 +14,13 @@ public class FirstCollaboration {
     /**
      * @param args the command line arguments
      */
+    static void robloxHelloWorld(){
+        System.out.println("robloxguy365 was here!");
+    }
     public static void main(String[] args) {
         // TODO code application logic here
+        robloxHelloWorld();
     }
+    
     
 }
